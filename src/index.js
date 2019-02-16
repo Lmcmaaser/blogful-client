d9d2c34eab35529376e5fc82cb8b499c5711fcd1
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2019-02-16 17:53:51 +0000

    everything except for restricting num login attmpts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFrog, faListUl, faListOl, faPenAlt, faGlobeAmericas, faBookOpen, faComment, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import * as serviceWorker from './serviceWorker'
+import { ArticleListProvider } from './contexts/ArticleListContext'
+import { ArticleProvider } from './contexts/ArticleContext'
+import App from './components/App/App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+library.add(
+  faFrog, // logo
+  faListUl, // style: listicle
+  faListOl, // style: howto
+  faGlobeAmericas, // style: news
+  faPenAlt, // style: interview
+  faBookOpen, // style: story
+  faComment,
+  faQuoteLeft,
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <ArticleListProvider>
+      <ArticleProvider>
+        <App />
+      </ArticleProvider>
+    </ArticleListProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
